269940cd1165ed7324537313d39811ae323b2cfa
mschrein <schreiner.marcel@web.de>
2021-12-04 19:04:23 +0100

    code simplification

--- day4/day4.R
+++ day4/day4.R
@@ -1,20 +1,23 @@
 
 # Part 1 ------------------------------------------------------------------
-# read in data
+# read in data (numbers and board data were saved in separate files)
 numbers <- as.numeric(read.table("day4/numbers.txt", sep = ",")[1,])
-boards <- read.table("day4/boards.txt")
-
+boards_data <- read.table("day4/boards.txt")
 # assign board ID
-boards$id <- as.factor(rep(1:(nrow(boards)/5), each = 5))
+boards_data$id <- as.factor(rep(1:(nrow(boards_data)/5), each = 5))
 
+boards <- boards_data # copy
+
+# play the game
 for(i in 1:length(numbers)){
   selection <- numbers[i]# draw numbers
-  boards[,-6][boards[,-6] == selection] <- NA # if selected number exists in boards, replace by NA
+  boards[,-6][boards[,-6] == numbers[i]] <- NA # if selected number exists in boards, replace by NA
   boards_split <- split(boards, f = boards$id) # split boards by id
-  row_check <- unlist(lapply(boards_split, FUN = function(x) 5 %in% rowSums(is.na(x)))) # check if 5 numbers have been selected for each row of each board
-  col_check <- unlist(lapply(boards_split, FUN = function(x) 5 %in% colSums(is.na(x)))) # check if 5 numbers have been selected for each column of each board
-  if(TRUE %in% row_check | TRUE %in% col_check){ # check if winner exists
-    winner_id <- ifelse(TRUE %in% row_check, yes = names(row_check[row_check]), no = names(col_check[col_check])) # select id of winning board
+  row_check <- sapply(boards_split, FUN = function(x) 5 %in% rowSums(is.na(x))) # check if 5 numbers have been selected for each row of each board
+  col_check <- sapply(boards_split, FUN = function(x) 5 %in% colSums(is.na(x))) # check if 5 numbers have been selected for each column of each board
+  winners <- row_check | col_check # for each board check whether it won
+  if(TRUE %in% winners){ # check if winner exists
+    winner_id <- names(winners)[winners] # select id of winning board
     winner <- boards[boards$id == winner_id, -6] # select winning board
     break
   }
@@ -24,18 +27,15 @@ sum(winner, na.rm = TRUE)*selection # sum non-selected values of winning board a
 
 # Part 2 ------------------------------------------------------------------
 
-# read in boards again
-boards <- read.table("day4/boards.txt")
-
-# assign board ID
-boards$id <- as.factor(rep(1:(nrow(boards)/5), each = 5))
+# reset boards
+boards <- boards_data
 
 for(i in 1:length(numbers)){
   selection <- numbers[i]# draw numbers
   boards[,-6][boards[,-6] == selection] <- NA # if selected number exists in boards, replace by NA
   boards_split <- split(boards, f = boards$id) # split boards by id
-  row_check <- unlist(lapply(boards_split, FUN = function(x) 5 %in% rowSums(is.na(x)))) # check if 5 numbers have been selected for each row of each board
-  col_check <- unlist(lapply(boards_split, FUN = function(x) 5 %in% colSums(is.na(x)))) # check if 5 numbers have been selected for each column of each board
+  row_check <- sapply(boards_split, FUN = function(x) 5 %in% rowSums(is.na(x))) # check if 5 numbers have been selected for each row of each board
+  col_check <- sapply(boards_split, FUN = function(x) 5 %in% colSums(is.na(x))) # check if 5 numbers have been selected for each column of each board
   winners <- row_check | col_check # for each board check whether it won
   if(length(winners[!winners]) == 1){ # check if only one board is left --> has to be last winner
     last_id <- names(winners[!winners]) # select id of last winning board
@@ -43,9 +43,8 @@ for(i in 1:length(numbers)){
   }
 }
 
-# read in boards again and select last winning board based on previously determined last_id
-boards <- read.table("day4/boards.txt")
-boards$id <- as.factor(rep(1:(nrow(boards)/5), each = 5))
+# reset boards and select last winning board based on previously determined last_id
+boards <- boards_data
 last <- boards[boards$id == last_id, -6]
 
 # play the game from the beginning for last board and determine selection with which last board wins
